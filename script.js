a8d4ebfdfc967b8f366fb374ddcabb015a00d4f4
Anthony Cecconi <95547209+sidra1991@users.noreply.github.com>
2022-02-25 14:21:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+//  l'utente da 2 numeri e bisogna specificare quale sia il magiore
+
+
+// const num1 = prompt(`numero1:`);
+// const num2 = prompt(`numero2:`);
+
+// if (num1 > num2){
+//  alert(" il numero magiore è: num1")
+// } else if (num1 < num2){
+//  alert(" il numero magiore è: num2")
+// }else {
+//  alert(" il numero magiore è: pari")
+// }
+
+// ---------------------------------------
+
+// l'utente inserisce 2 parole in successione con 2 prompt. il software stampa prima la parola più lunga, poi la più corta
+
+// const parola1 = prompt("inserisci la prima parola");
+// const parola2 = prompt( " inserisci la seconda parola" );
+
+// if (parola1.length > parola2.length){
+//  alert(`${ parola1 } + ${ parola2 } `)
+// } else if (parola1.length < parola2.length){
+//  alert(`${ parola2 } + ${ parola1 } `)
+// }else {
+//  alert(`${ parola1 } + ${ parola2 } sono lunghe uguali`)
+// }
+
+// --------------------------------------
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+//   let num = 0;
+
+//   for (let i = 0; i < 10; i++) {
+//       const newNum = parseInt(prompt(`inserisci numero`));
+//       num += newNum;
+//     }
+
+//   alert(`la somma è ${num}`)
+
+// -------------------------------------
+
+// Snack 4
+// In un array sono contenuti i nomi degli invitati alla festa del grande
+// Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+// no alla festa.
+
+ const invited
+
+
+
+
+
+
+// Snack 5
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+// Snack 6
+// Chiedi un numero di 4 cifre all’utente
+// e calcola la somma di tutte le cifre che compongono il numero.
+
