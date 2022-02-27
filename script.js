3ce42c892c0ddc04dbc519f3ed9be20db2597b14
sidra1991 <1991sidra@gmail.com>
2022-02-27 15:33:00 +0100

    daje

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 //  l'utente da 2 numeri e bisogna specificare quale sia il magiore
 
 
-// const num1 = prompt(`numero1:`);
-// const num2 = prompt(`numero2:`);
+ function snackFunction1(){
 
-// if (num1 > num2){
-//  alert(" il numero magiore è: num1")
-// } else if (num1 < num2){
-//  alert(" il numero magiore è: num2")
-// }else {
-//  alert(" il numero magiore è: pari")
-// }
+    let num1 = document.getElementById('snackFirstOne').value;
+    let num2 = document.getElementById('snackFirstTwo').value;
+
+
+    if (num1 > num2){
+        document.getElementById('snackFirstTree').innerHTML=`il valore maggiore è ${num1} del primo valore`;
+    } else if (num1 < num2){
+        document.getElementById('snackFirstTree').innerHTML=`il valore maggiore è ${num2} del secondo valore`;
+    }else {
+        document.getElementById('snackFirstTree').innerHTML='sono valori di pari valore';
+    }           
+}
 
 // ---------------------------------------
 
 // l'utente inserisce 2 parole in successione con 2 prompt. il software stampa prima la parola più lunga, poi la più corta
 
-// const parola1 = prompt("inserisci la prima parola");
-// const parola2 = prompt( " inserisci la seconda parola" );
 
-// if (parola1.length > parola2.length){
-//  alert(`${ parola1 } + ${ parola2 } `)
-// } else if (parola1.length < parola2.length){
-//  alert(`${ parola2 } + ${ parola1 } `)
-// }else {
-//  alert(`${ parola1 } + ${ parola2 } sono lunghe uguali`)
-// }
+function snackFunction2(){
+    let word1 = document.getElementById('snackTwoOne').value;
+    let word2 = document.getElementById('snackTwoTwo').value;
 
+    if (word1.length > word2.length){
+        document.getElementById('snackTwoTree').innerHTML=`la parola più lunga è :${word1} la più corta è :${word2}`;
+    } else if (word1.length < word2.length){
+        document.getElementById('snackTwoTree').innerHTML=`la parola più lunga è :${word2} la più corta è :${word1}`;
+    }else {
+        document.getElementById('snackTwoTree').innerHTML=`le parole sono lunghe uguali`;
+    }
+}
 // --------------------------------------
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
@@ -90,4 +96,13 @@
 // Chiedi un numero di 4 cifre all’utente
 // e calcola la somma di tutte le cifre che compongono il numero.
 
-alert("mi manca solo l'ultimo snack ma vado a prendere il bambino a squola, finiti gli snack li indentero esteticamente")
+/*const userNumber = prompt("dammi un numero di 4 cifre");
+let newNumber = 0;
+
+for (let separated = 0; separated < userNumber.length; separated++) {
+    newNumber += parseInt(userNumber[separated]);
+}
+
+alert(`la somma della tua cifra è ${newNumber}`) */
+
+
